95821183fa5efaaf4df11b93ca57fe44e003a906
Julien SCAJOLA <julien.scajola@hotmail.com>
2015-01-30 16:38:30 +0100

    Ajout des commentaires pour la classe FISH. Cette classe est a prendre comme modéle pour les commentaires des fichier En-tête

--- FISH-OPENROV/model/fish.h
+++ FISH-OPENROV/model/fish.h
@@ -1,20 +1,76 @@
 #ifndef FISH_H
 #define FISH_H
 
+/*! 
+ * \file Fish.h
+ */
+
 #include <QObject>
+
+/*! 
+ * \class Fish fish.h
+ * \brief Classe retprésentant un poisson
+ */
 
 class Fish : public QObject
 {
     Q_OBJECT
 public:
+    /*!
+     *  \brief Constructeur
+     *      Constructeur de la classe Fish
+     *  \param parent : Objet parent
+     */
     explicit Fish(QObject *parent = 0);
+    /*!
+     *  \brief Constructeur
+     *      Constructeur de la classe Fish
+     *  \param int _id : id du poisson
+     *  \param QString _name : nom du poisson
+     *  \param double _size : taille du poisson
+     */
     Fish(int _id, QString _name, double _size);
     
+    /*!
+     *  \brief GetId()
+     *      Fonction qui permet de récupérer l'id d'un poisson
+     *  \return int
+     */
     int GetId();
+    
+    /*!
+     *  \brief GetName()
+     *      Fonction qui permet de récupérer le nom d'un poisson
+     *  \return QString
+     */
     QString GetName();
+    
+    /*!
+     *  \brief GetSize()
+     *      Fonction qui permet de récupérer la taille d'un poisson
+     *  \return double
+     */
     double GetSize();
+    
+    /*!
+     *  \brief SetId()
+     *      Méthode qui permet de setter la taille d'un poisson
+     *  \param int _id : id du poisson
+     */
     void SetId (int _id);
+    
+    /*!
+     *  \brief SetName()
+     *      Méthode qui permet de setter le nom d'un poisson
+     *  \param QString _name : nom du poisson
+     */
     void SetName(QString _name);
+    
+    /*!
+     *  \brief SetSize()
+     *      Méthode qui permet de setter la taille d'un poisson
+     *  \param double _size : taille du poisson
+     */
     void SetSize(double _size);
     
 private:
