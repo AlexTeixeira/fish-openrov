9fd92ad70c8ccf1b65eed29515666d78ee4f7be2
Antony Veyre <veyre@icloud.com>
2015-02-08 22:24:12 +0100

    video_tst implementation (debut)

--- test/main_tst.cpp
+++ test/main_tst.cpp
@@ -3,6 +3,7 @@
 #include <fish_tst.h>
 #include <frame_tst.h>
 #include <result_tst.h>
+#include <video_tst.h>
 
 int main()
 {
@@ -12,4 +13,6 @@ int main()
     QTest::qExec(&frt);
     ResultTest rst;
     QTest::qExec(&rst);
+    VideoTest vit;
+    QTest::qExec(&vit);
 }
