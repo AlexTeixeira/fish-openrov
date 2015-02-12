1802a87c45b1a47fd7ffe71a5c089002d3e7ce8c
Antony Veyre <veyre@icloud.com>
2015-02-12 11:35:41 +0100

    intendation

--- test/place_tst.cpp
+++ test/place_tst.cpp
@@ -9,23 +9,23 @@ PlaceTest::PlaceTest(QObject *parent) : QObject(parent)
 void PlaceTest::initTestCase()
 {
 
-p1 = new Place();
-p2 = new Place (3,"video name");
+        p1 = new Place();
+        p2 = new Place (3,"video name");
 }
 
 void PlaceTest::testId()
 {
-p1->SetId(4579);
-QVERIFY(p1->GetId()==4579);
-QVERIFY(p2->GetId()==3);
+        p1->SetId(4579);
+        QVERIFY(p1->GetId()==4579);
+        QVERIFY(p2->GetId()==3);
 }
 
 void PlaceTest::testName()
 {
-    p1->SetName("p1");
-    QVERIFY(p1->GetName()=="p1");
-    QVERIFY(p2->GetName()=="video name");
-    }
+        p1->SetName("p1");
+        QVERIFY(p1->GetName()=="p1");
+        QVERIFY(p2->GetName()=="video name");
+}
 
 
 
@@ -34,9 +34,7 @@ void PlaceTest::testListFish()
 
     Fish *f3 = new Fish(3,"name fish",54.32);
     p1->SetListFish(new QList<Fish>());
-        //    p1->AddFish(f3);
-
-
+           p1->AddFish(f3);
 }
 
 void PlaceTest::cleanupTestCase()
